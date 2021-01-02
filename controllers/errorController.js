42a083bfe5d96f1bb48c55c9a984c7e18ccf6a29
Okegbemi Olatunde <okegbemiolatunde@gmail.com>
2021-01-02 03:02:03 +0100

    Updated Changes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,14 +6,18 @@ const handleCastErrorDB = err => {
 }
 
 const handleDuplicateFieldsDB = err => {
-    const message = `Duplicate field value. Please use another value`
-    return new AppError(message, 400) 
-}
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  
+    const message = `Duplicate field value: ${value}. Please use another value!`;
+    return new AppError(message, 400);
+};
 
 const handleValidationErrorDB = err => {
-    const message = `Invalid input Data`
-    return new AppError(message, 400) 
-}
+    const errors = Object.values(err.errors).map(el => el.message);
+
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+};
 
 const handleJWTError = () => new AppError('Invalid Token. Please log in again', 401)
 const handleJWTExpiredError = () => new AppError('Your Token has expired. Please log in again', 401)
@@ -72,7 +76,7 @@ module.exports = (err, req, res, next) => {
         if(error.name === 'JsonWebTokenError') error = handleJWTError()
         if(error.name === 'TokenExpiredError') error = handleJWTExpiredError()
         
-        sendErrorProd(error, res)
+        sendErrorProd(error, req, res)
     }
 
 }
