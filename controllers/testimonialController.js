b7df79ac5c77964c893e82431df6954c5ae3070c
Okegbemi Olatunde <okegbemiolatunde@gmail.com>
2021-01-04 18:16:25 +0100

    Updated Changes

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -50,7 +50,7 @@ exports.createTestimonial = catchAsync(async (req, res, next) => {
     if (!req.file) return next();
 
     const fileName = `testimonial-${Date.now()}.jpeg`;
-    req.body.image = process.env.AWS_URL+'/testimonials-images/'+fileName;
+    req.body.image = process.env.AWS_URL+'testimonials-images/'+fileName;
 
     const testimonial = await Testimonial.create(req.body)
     uploadFileToS3(req, fileName)
@@ -67,7 +67,7 @@ exports.updateTestimonial = catchAsync(async (req, res, next) => {
 
     if(req.file){
         const fileName = `testimonial-${Date.now()}.jpeg`;
-        req.body.image = process.env.AWS_URL+'/testimonials-images/'+fileName;
+        req.body.image = process.env.AWS_URL+'testimonials-images/'+fileName;
         uploadFileToS3(req, fileName)
     }
 
