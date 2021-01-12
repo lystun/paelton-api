844bf230469cf987b513ff0fd8281622023a6c16
Okegbemi Olatunde <okegbemiolatunde@gmail.com>
2021-01-12 16:31:10 +0100

    Updated Changes

--- utils/aws.js
+++ utils/aws.js
@@ -1,13 +1,25 @@
 const AWS = require('aws-sdk');
 
+let awsS3;
+
+
 const formatCredentials = (value) => {
     return value.replace(/[",]+/g,'')
 }
 
-const awsS3 = new AWS.S3({
-    accessKeyId: formatCredentials(process.env.AWS_ACCESS_KEY_ID),
-    secretAccessKey: formatCredentials(process.env.AWS_SECRET_KEY),
-})
+if (process.env.NODE_ENV === 'development'){
+    awsS3 = new AWS.S3({
+        accessKeyId: formatCredentials(process.env.AWS_ACCESS_KEY_ID),
+        secretAccessKey: formatCredentials(process.env.AWS_SECRET_KEY),
+    })
+}
+
+if (process.env.NODE_ENV === 'production'){
+    awsS3 = new AWS.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+    })
+}
 
 
 module.exports = awsS3
